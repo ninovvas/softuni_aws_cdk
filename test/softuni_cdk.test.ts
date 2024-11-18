949ebb3cefefa031374fc2e2d67622b22f48be43
unknown <ninov.vasil@googlemail.com>
2024-11-18 18:52:20 +0100

    Initial commit

---
+++ test/softuni_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SoftuniCdk from '../lib/softuni_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/softuni_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SoftuniCdk.SoftuniCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
